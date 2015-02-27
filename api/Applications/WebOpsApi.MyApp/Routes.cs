9a03c192468d58b23213778e91e27bc8c721b472
Daniel Lewis <daniel_lewis@esri.com>
2015-02-27 15:02:22 -0800

    added sample app to api

---
+++ api/Applications/WebOpsApi.MyApp/Routes.cs
@@ -0,0 +1,20 @@
+﻿
+using WebOpsApi.Shared;
+
+namespace WebOpsApi.MyApp
+{
+	public class Routes : ApiRoutingTable
+	{
+		public Routes()
+		{
+			Get["/"] = ctx =>
+			{
+				return new ApiResponse
+				{
+					Success = true,
+					Data = "Welcome To My App"
+				};
+			};
+		}
+	}
+}

ebf11b1b7b0da33c69a2329fb5a52d63babeb6e0
Daniel Lewis <daniel_lewis@esri.com>
2015-03-04 15:31:09 -0800

    working on api example

--- api/Applications/WebOpsApi.MyApp/Routes.cs
+++ api/Applications/WebOpsApi.MyApp/Routes.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Generic;
+using System.Linq;
 using WebOpsApi.Shared;
 
 namespace WebOpsApi.MyApp
@@ -7,14 +9,57 @@ namespace WebOpsApi.MyApp
 	{
 		public Routes()
 		{
-			Get["/"] = ctx =>
+			Get["/"] = ctx => new ApiResponse
 			{
+				Success = true,
+				Data = "Welcome To My App"
+			};
+
+			//*************************************************
+			//	Example REST Implementation
+			//*************************************************
+
+			var things = new List<string> { "Thing 1", "Thing 2" };
+
+			Get["/Things"] = ctx => new ApiResponse
+			{
+				Success = true,
+				Data = things
+			};
+
+			Post["/Things"] = ctx =>
+			{
+				var thing = ctx.ReadParam("thing").ToString();
+				if (!string.IsNullOrWhiteSpace(thing))
+				{
+					things.Add(thing);
+
+					return new ApiResponse
+					{
+						Success = true,
+						Data = thing + " Added to collection"
+					};
+				}
+
 				return new ApiResponse
 				{
-					Success = true,
-					Data = "Welcome To My App"
+					Success = false,
+					ErrorMessage = "No Thing In Post Data!"
 				};
+
 			};
+
+			Put["/Things"] = ctx =>
+			{
+				var pk = ctx.ReadParam("pk").ToString();  //In this example we use the previous value
+				var thing = ctx.ReadParam("thing").ToString();
+
+				var thingToUpdate = things.FirstOrDefault(x => x == pk);
+
+				return null;
+
+			};
+
 		}
 	}
 }
